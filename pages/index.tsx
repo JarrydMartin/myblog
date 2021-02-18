a0c54ac093b029910df219b0e71fa3feffd4ab14
Jarryd Martin <jarryd@codora.co.nz>
2021-02-18 23:34:55 +1300

    Centered waiting image and error image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,39 @@
-import Image from 'next/image'
-import {useUser} from '@auth0/nextjs-auth0';
-import React from 'react';
-import { Grid } from '@material-ui/core';
-
+import Image from "next/image";
+import { useUser } from "@auth0/nextjs-auth0";
+import React from "react";
+import { Grid } from "@material-ui/core";
 
 export default function Home() {
-    const { isLoading, error} = useUser();
+  const { isLoading, error } = useUser();
 
-    if (isLoading) return <Loading/>;
-    
-    if (error) return <Error/>
+  if (isLoading) return <Loading />;
 
-    return (
-        <Grid
-  container
-  direction="column"
-  justify="center"
-  alignItems="center"
->
-            <h1>Coming Soon...</h1>
-            <Image src="/Charco/Charco/Coming_soon.png"
-                width={500}
-                height={500}/>
-        </Grid>
-    )
+  if (error) return <Error />;
+
+  return (
+    <Grid container direction="column" justify="center" alignItems="center">
+      <h1>Coming Soon...</h1>
+      <Image src="/Charco/Charco/Coming_soon.png" width={500} height={500} />
+    </Grid>
+  );
 }
 
 function Loading() {
-    return (
-        <div>
-            <Image src="/Charco/Charco/Waiting.png"
-                width={500}
-                height={500}/>
-        </div>
-    )
+  return (
+    <Grid container direction="column" justify="center" alignItems="center">
+      <Image src="/Charco/Charco/Waiting.png" width={500} height={500} />
+    </Grid>
+  );
 }
 
 function Error() {
-    return <Image src="/Charco/Charco/Something_went_wrong.png"
+  return (
+    <Grid container direction="column" justify="center" alignItems="center">
+      <Image
+        src="/Charco/Charco/Something_went_wrong.png"
         width={500}
-        height={500}/>
+        height={500}
+      />
+    </Grid>
+  );
 }
