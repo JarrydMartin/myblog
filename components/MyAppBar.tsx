010ddccb06d81e15ccb523994e8534976531bf1e
Jarryd Martin <jarryd@codora.co.nz>
2021-02-20 13:08:56 +1300

    added css crid to app bar

--- components/MyAppBar.tsx
+++ components/MyAppBar.tsx
@@ -3,14 +3,17 @@ import React from "react";
 import { useUser } from "@auth0/nextjs-auth0";
 
 const useStyles = makeStyles({
-  root: {
-    // background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-    // border: 0,
-    // borderRadius: 3,
+  root:  {
+    display: "grid",
+    gridTemplateColumns: "200px auto 200px",
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    // color: 'white',
-    // height: 48,
-    // padding: '0 30px',
+  },
+  title:{
+    gridColumn: "1/2"
+  },
+  authButton: {
+    gridColumn: "3/4",
+    justifySelf: "end"
   },
   button : {
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
@@ -41,16 +44,12 @@ function LogoutButton() {
   return (
     <AppBar position="static">
       <Toolbar className={classes.root}>
-       <Grid container spacing={5}>
-         <Grid item xs={11}>
-          <Typography variant="h6">
-          OpenClose Bracket
+          <Typography className={classes.title}variant="h6">
+           OpenClose Bracket
           </Typography>
-          </Grid>
-             <Grid item xs>
+          <div className={classes.authButton}>
           {user ? <LogoutButton /> : <LoginButton/>}
-          </Grid>
-        </Grid>
+          </div>
       </Toolbar>
     </AppBar>
   );
